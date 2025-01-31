cf7e6429cc8fb8d34421b07569c788d500b89869
Alex Arbogast <arbogastaw@gmail.com>
2025-01-31 18:19:08 -0500

    add orientation to moves

--- scripts/control_demo.py
+++ scripts/control_demo.py
@@ -1,17 +1,26 @@
 import numpy as np
+import quaternion
 import rospy
-
-from geometry_msgs.msg import Quaternion
 
 from taskspace_control_examples import ControlDemo
 from taskspace_control_examples.trajectory import *
+
+robot_params = {
+    "pose_controller": {
+        "home": [0.484, 0.425, 0.661, 1.959, -1.77, -0.446],
+    },
+    "as_nullspace_controller": {
+        "home": [0.484, 0.425, 0.661, 1.959, -1.77, -0.446],
+    },
+}
 
 
 class BaseframeControlDemo(ControlDemo):
     def __init__(self, setpoint_hz=1000):
         super(BaseframeControlDemo, self).__init__(setpoint_hz)
-        self.static_orient = Quaternion(0, 0, 0, 1)
-        self.home = [0.238, 0.496, 0.568, 1.498, -1.531, -0.505]
+        controller_type = rospy.get_param("~controller", "pose_controller")
+        self.home = robot_params[controller_type]["home"]
+        self.static_orient = np.quaternion(1.0, 0.0, 0.0, 0.0)
         self.arm_id = rospy.get_param("~arm_id")
 
     def run(self):
@@ -20,6 +29,7 @@ class BaseframeControlDemo(ControlDemo):
 
         self.start_taskspace_control()
 
+        self.test_line()
         self.test_cube()
         self.base_frame_circle()
         self.base_frame_hypotrochoid()
@@ -27,36 +37,51 @@ class BaseframeControlDemo(ControlDemo):
         self.start_joint_control()
         self.joint_controller_client.move_joint(self.home, 3.0)
 
+    def test_line(self):
+        tf = 3
+        p_start = np.array([0.5, 0.3, 0.1])
+        p_end = np.array([0.5, -0.3, 0.1])
+        self.path_viz.visualize_path([p_start, p_end], f"{self.arm_id}_base_link")
+
+        self.movel(p_start, self.static_orient, 3)
+        self.execute_linear_path(
+            p_start, p_end, self.static_orient, self.static_orient, tf
+        )
+        self.path_viz.reset()
+
     def test_cube(self):
-        center = np.array([0.5, 0.0, 0.5])
+        center = np.array([0.4, 0.0, 0.35])
         points = np.array(
             [
                 [0.0, 0.0, 0.0],
-                [0.0, 0.5, -0.15],
-                [-1.0, 0.5, -0.15],
-                [-1.0, 0.5, 0.50],
-                [0.0, 0.5, 0.50],
-                [0.0, -0.5, 0.50],
-                [-1.0, -0.5, 0.50],
-                [-1.0, -0.5, -0.15],
-                [0.0, -0.5, -0.15],
-                [0.0, 0.5, -0.15],
+                [-0.05, 0.5, -0.25],
+                [-1.0, 0.5, -0.25],
+                [-1.0, 0.5, 0.45],
+                [-0.05, 0.5, 0.45],
+                [-0.05, -0.5, 0.45],
+                [-1.0, -0.5, 0.45],
+                [-1.0, -0.5, -0.25],
+                [-0.05, -0.5, -0.25],
+                [-0.05, 0.5, -0.25],
             ]
         )
         points += center
         self.path_viz.visualize_path(points[1:], f"{self.arm_id}_base_link")
 
+        self.movel(center, self.static_orient, 2)
         for i in range(len(points) - 1):
             current = points[i]
             next = points[i + 1]
-            self.execute_linear_path(current, next, 2.0)
+            self.execute_linear_path(
+                current, next, self.static_orient, self.static_orient, 2.0
+            )
 
         self.path_viz.reset()
 
     def base_frame_circle(self):
         tf = 5
         tt = np.linspace(0, tf, int(self.hz * tf))
-        f, f_dot = circular_traj(1 / 5, tf)
+        f, f_dot = circular_traj(1 / 7, tf)
 
         offset = np.array([0.5, 0.0, 0.2])
         ft, f_dott = f(tt) + offset, f_dot(tt)
@@ -66,17 +91,17 @@ class BaseframeControlDemo(ControlDemo):
             f"{self.arm_id}_base_link",
         )
 
-        self.movel(ft[0], 2)
-        self.execute_path(ft, f_dott)
+        self.movel(ft[0], self.static_orient, 2)
+        self.execute_path(ft, f_dott, self.static_orient)
         self.path_viz.reset()
 
     def base_frame_hypotrochoid(self):
-        scale = 1 / 28
+        scale = 1 / 30
         tf = 10
         tt = np.linspace(0, tf, int(self.hz * tf))
         f, f_dot = hypotrochoid_traj(3, 5, 4.5, tf, scaling=Order.THIRD)
 
-        offset = np.array([0.5, 0.0, 0.2])
+        offset = np.array([0.45, 0.0, 0.2])
         ft, f_dott = scale * f(tt) + offset, scale * f_dot(tt)
 
         self.path_viz.visualize_path(
@@ -84,8 +109,8 @@ class BaseframeControlDemo(ControlDemo):
             f"{self.arm_id}_base_link",
         )
 
-        self.movel(ft[0], 1)
-        self.execute_path(ft, f_dott)
+        self.movel(ft[0], self.static_orient, 1)
+        self.execute_path(ft, f_dott, self.static_orient)
         self.path_viz.reset()
 
 
